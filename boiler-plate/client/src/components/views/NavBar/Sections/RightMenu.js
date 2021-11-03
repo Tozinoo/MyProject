3f76b5c68b60504ee25d5f4e1b19e3f9831228d2
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-11-04 02:08:59 +0900

    Update RightMenu.js

--- boiler-plate/client/src/components/views/NavBar/Sections/RightMenu.js
+++ boiler-plate/client/src/components/views/NavBar/Sections/RightMenu.js
@@ -4,6 +4,7 @@ import { Menu } from "antd";
 import axios from "axios";
 import { USER_SERVER } from "../../../../Config";
 import { withRouter } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 
 function RightMenu(props) {
