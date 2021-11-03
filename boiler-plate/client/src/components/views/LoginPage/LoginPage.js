6980280b8c02131000276d179d0e23243c578458
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-11-04 02:43:38 +0900

    register/login

--- boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -2,6 +2,39 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { loginUser } from "../../../actions/user_actions";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props) {
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {"Copyright © "}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
 
 const LoginPage = (props) => {
     const dispatch = useDispatch();
@@ -27,6 +60,7 @@ const LoginPage = (props) => {
             if (response.payload.loginSuccess) {
                 props.history.push("/");
             } else {
+                console.log(body.email);
                 alert("Error");
             }
         });
@@ -42,7 +76,88 @@ const LoginPage = (props) => {
                 height: "100vh",
             }}
         >
-            <form
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box
+                            component="form"
+                            onSubmit={onSubmitHandler}
+                            noValidate
+                            sx={{ mt: 1 }}
+                        >
+                            <TextField
+                                value={Email}
+                                onChange={onEmailHandler}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                value={Password}
+                                onChange={onPasswordHandler}
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        value="remember"
+                                        color="primary"
+                                    />
+                                }
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 8, mb: 4 }} />
+                </Container>
+            </ThemeProvider>
+            {/* <form
                 style={{ display: "flex", flexDirection: "column" }}
                 onSubmit={onSubmitHandler}
             >
@@ -56,7 +171,7 @@ const LoginPage = (props) => {
                 />
                 <br />
                 <button>Login</button>
-            </form>
+            </form> */}
         </div>
     );
 };
