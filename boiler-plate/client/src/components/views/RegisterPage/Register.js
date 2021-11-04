5a3724f2dfa22cca1b4ec48fa09dda13d4870dab
Tozinoo <goldboy122@gmail.com>
2021-11-04 16:22:33 +0900

    boiler-plate before validatation

--- boiler-plate/client/src/components/views/RegisterPage/Register.js
+++ boiler-plate/client/src/components/views/RegisterPage/Register.js
@@ -20,10 +20,11 @@ const SignUp = () => {
     console.log(watch('email'));
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="test_register">
+            <form className="test_form" onSubmit={handleSubmit(onSubmit)}>
                 <label>Email</label>
                 <input
+                    className="test_input"
                     name="email"
                     type="email"
                     {...register('email', {
@@ -31,36 +32,45 @@ const SignUp = () => {
                         pattern: /^\S+@\S+$/i,
                     })}
                 />
-                {errors.email && <p>This email field is required</p>}
+                {errors.email && (
+                    <p className="test_error">This email field is required</p>
+                )}
 
                 <label>Name</label>
                 <input
+                    className="test_input"
                     name="name"
                     type="text"
                     {...register('name', { required: true, maxLength: 10 })}
                 />
                 {errors.name && errors.name.type === 'required' && (
-                    <p>This field is required</p>
+                    <p className="test_error">This field is required</p>
                 )}
                 {errors.name && errors.name.type === 'maxLength' && (
-                    <p>Your input exceed maximum length</p>
+                    <p className="test_error">
+                        Your input exceed maximum length
+                    </p>
                 )}
 
                 <label>Password</label>
                 <input
+                    className="test_input"
                     name="password"
                     type="password"
                     {...register('password', { required: true, minLength: 6 })}
                 />
                 {errors.password && errors.password.type === 'required' && (
-                    <p>This field is required</p>
+                    <p className="test_error">This field is required</p>
                 )}
                 {errors.password && errors.password.type === 'minLength' && (
-                    <p>Password must have at least 6 characters</p>
+                    <p className="test_error">
+                        Password must have at least 6 characters
+                    </p>
                 )}
 
                 <label>Password Confirm</label>
                 <input
+                    className="test_input"
                     name="password_confirm"
                     type="password"
                     {...register('password_confirm', {
@@ -71,11 +81,11 @@ const SignUp = () => {
 
                 {errors.password_confirm &&
                     errors.password_confirm.type === 'required' && (
-                        <p>This field is required</p>
+                        <p className="test_error">This field is required</p>
                     )}
                 {errors.password_confirm &&
                     errors.password_confirm.type === 'validate' && (
-                        <p>The passwords do not match</p>
+                        <p className="test_error">The passwords do not match</p>
                     )}
 
                 <input type="submit" />
